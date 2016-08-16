8b45e519ce636903e06bfb79e388284776c2c11d
William Otávio Souto <williamotavio@gmail.com>
2016-08-16 01:34:36 -0300

    First up

---
+++ www/js/app.js
@@ -0,0 +1,35 @@
+(function() {
+
+var app = angular.module('myreddit', ['ionic' , 'angularMoment']);
+
+app.controller('RedditCtrl', function($http, $scope) {
+
+  $scope.stories = [];
+
+  $http.get('https://www.reddit.com/r/Android/new/.json')
+    .success(function(response){
+      angular.forEach(response.data.children, function(child){
+        $scope.stories.push(child.data);
+      });
+    });
+
+
+});
+
+
+
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());
