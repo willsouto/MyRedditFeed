87f872f69ec8d09c3919407e9de06f799c52a8f9
William Otávio Souto <williamotavio@gmail.com>
2016-08-20 13:04:50 -0300

    refresh scroll up e down

--- www/js/app.js
+++ www/js/app.js
@@ -3,20 +3,54 @@
 var app = angular.module('myreddit', ['ionic' , 'angularMoment']);
 
 app.controller('RedditCtrl', function($http, $scope) {
-
   $scope.stories = [];
+ 
 
-  $http.get('https://www.reddit.com/r/Android/new/.json')
-    .success(function(response){
-      angular.forEach(response.data.children, function(child){
-        $scope.stories.push(child.data);
+  /** load de historia **/
+  function loadStories(params, callback){
+      $http.get('https://www.reddit.com/r/funny/new/.json', {params:params})
+        .success(function(response){
+          var stories = [];
+          angular.forEach(response.data.children, function(child){
+            var story = child.data;
+            if (!story.thumbnail || story.thumbnail === 'self' || story.thumbnail === 'default'){
+              story.thumbnail = 'http://www.redditstatic.com/icon.png';
+            }
+            else if(story.thumbnail === 'nsfw'){
+               story.thumbnail = 'http://i.imgur.com/UHzw6.png';
+            }
+           stories.push(child.data);
+          });
+          callback(stories);
+        });
+  }
+
+  /** load de historia antiga no scroll down **/
+    $scope.loadOlderStories = function(){
+      var params = {};
+      if($scope.stories.length > 0){
+        params['after'] = $scope.stories[$scope.stories.length - 1].name;
+      }
+      loadStories(params , function(olderStories){
+        $scope.stories = $scope.stories.concat(olderStories);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
       });
-    });
+    };
 
+
+  /** load de historia recent no scroll up **/
+    $scope.loadNewerStories = function(){
+      var params = {'before': $scope.stories[0].name};
+      loadStories(params, function(newerStories){
+        $scope.stories = newerStories.concat($scope.stories);
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    };
+    $scope.openLiunk = function(url){
+      windows.open(url, '_blank');
+    };
 
 });
-
-
 
 
 
@@ -26,6 +60,7 @@ app.run(function($ionicPlatform) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
